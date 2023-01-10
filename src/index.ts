9e3f0d733d182dea6b2482d928a75307a6e2d100
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-01-10 21:42:54 +0300

    Improves ts api generator

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import '@infinite-debugger/rmk-js-extensions/String';
 
-import { OpenSpec3 } from '@tsed/openspec';
+import { OS3Parameter, OS3Paths, OpenSpec3 } from '@tsed/openspec';
+
+type Parameter = {
+  name: string;
+  type: string;
+};
 
 interface APIAction {
   name: string;
   endpointPathIdentifierString: string;
   enpointPathString: string;
   httpVerb: string;
-  parameters: string[];
+  pathParams: Parameter[];
   snippet: string;
+  actionDescription: string;
 }
 
 interface APIEntity {
@@ -19,14 +25,20 @@ const swaggerDocs: OpenSpec3 = require('../swagger.json');
 
 // Cumulatively finding entites
 const entities = Object.keys(swaggerDocs.paths).reduce(
-  (accumulator, pathKey) => {
-    Object.keys(swaggerDocs.paths[pathKey]).forEach((httpVerb) => {
-      const { summary, tags } = (swaggerDocs.paths as any)[pathKey][
-        httpVerb
-      ] as {
-        summary?: string;
-        tags?: string[];
+  (accumulator, pathKey: keyof typeof swaggerDocs.paths) => {
+    const swaggerDocsPath = swaggerDocs.paths[pathKey];
+    Object.keys(swaggerDocsPath).forEach((key) => {
+      const httpVerb = key as keyof typeof swaggerDocsPath;
+      const {
+        summary,
+        tags,
+        parameters = [],
+      } = {
+        ...(swaggerDocsPath[httpVerb] as any),
+      } as OS3Paths & {
+        tags: string[];
       };
+
       if (tags && tags.length > 0 && summary) {
         const entityGroupName = tags[0];
         if (!accumulator[entityGroupName]) {
@@ -38,18 +50,40 @@ const entities = Object.keys(swaggerDocs.paths).reduce(
         const endpointPathIdentifierString = `${summary
           .replace(/\s+/g, '_')
           .toUpperCase()}_ENDPOINT_PATH`;
+        const [verb, ...restSummary] = summary.split(' ');
+        const actionDescription =
+          verb.replace(/[ei]+$/g, '') + 'ing ' + restSummary.join(' ');
         const enpointPathString = pathKey.replace(/\{(\w+)\}/g, ':$1');
+        const pathParams = parameters
+          .filter((baseParameter) => {
+            const parameter = baseParameter as OS3Parameter;
+            return parameter.in && parameter.in === 'path';
+          })
+          .map((baseParameter) => {
+            const parameter = baseParameter as OS3Parameter;
+            return {
+              name: parameter.name,
+              type: (parameter.schema as any).type, // TODO: Deal with complex types
+            } as Parameter;
+          });
+
+        const pathParamsString = pathParams
+          .map(({ name, type }) => {
+            return `${name}: ${type}`;
+          })
+          .join(', ');
 
         accumulator[entityGroupName].actions.push({
           name,
           httpVerb,
           enpointPathString,
           endpointPathIdentifierString,
-          parameters: [],
+          actionDescription,
+          pathParams,
           snippet: `
-            export const ${name} = async () => {
+            export const ${name} = async (${pathParamsString}) => {
               const { data } = await ${httpVerb}<Country[]>(${endpointPathIdentifierString}, {
-                label: 'Loading Countries',
+                label: '${actionDescription}',
               });
               return data;
             };
