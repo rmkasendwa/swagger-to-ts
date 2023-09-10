8386788b41fc55adfd2fac79d02012b3ab2efe2b
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-09-10 03:16:45 +0300

    Refactors SchemaGenerator

--- src/api/SchemaGenerator.ts
+++ src/api/SchemaGenerator.ts
@@ -4,97 +4,57 @@ import { Schema } from '../models/OpenAPISpecification/Schema';
 
 //#region Find all schemas referenced by a schema
 export interface FindSchemaReferencedSchemasOptions {
+  /**
+   * The OpenAPI specification to search for referenced schemas.
+   */
   openAPISpecification: OpenAPISpecification;
+
+  /**
+   * The name of the schema to find all referenced schemas for.
+   */
   schemaName: string;
 }
+
+/**
+ * Find all schemas referenced by a schema.
+ *
+ * @param options The options to find all referenced schemas for a schema.
+ * @returns All schemas referenced by the schema.
+ */
 export const findSchemaReferencedSchemas = ({
   schemaName,
   openAPISpecification,
 }: FindSchemaReferencedSchemasOptions) => {
   const schemaReferencedSchemas: string[] = [];
-  const findSchemaReferencedBySchemaProperty = (property: Schema) => {
-    if ('type' in property) {
-      switch (property.type) {
-        case 'array':
-          {
-            if (property.items && '$ref' in property.items) {
-              const schemaName = property.items.$ref.replace(
-                '#/components/schemas/',
-                ''
-              );
-              if (!schemaReferencedSchemas.includes(schemaName)) {
-                schemaReferencedSchemas.unshift(schemaName);
-                findSchemaReferencedSchemasRecursive(schemaName);
-              }
-            }
-          }
-          break;
-        case 'object':
-          {
-            if (property.properties) {
-              Object.values(property.properties).forEach((property) => {
-                if (
-                  typeof property === 'object' &&
-                  property &&
-                  '$ref' in property &&
-                  typeof property.$ref === 'string'
-                ) {
-                  const schemaName = property.$ref.replace(
-                    '#/components/schemas/',
-                    ''
-                  );
-                  if (!schemaReferencedSchemas.includes(schemaName)) {
-                    schemaReferencedSchemas.unshift(schemaName);
-                    findSchemaReferencedSchemasRecursive(schemaName);
-                  }
-                }
-              });
-            }
-          }
-          break;
-      }
-    }
-    if ('$ref' in property) {
-      const schemaName = property.$ref.replace('#/components/schemas/', '');
-      if (!schemaReferencedSchemas.includes(schemaName)) {
-        schemaReferencedSchemas.unshift(schemaName);
-        findSchemaReferencedSchemasRecursive(schemaName);
-      }
-    }
-  };
 
+  /**
+   * Find all referenced schemas recursively.
+   *
+   * @param schemaName The name of the schema to find all referenced schemas for.
+   */
   const findSchemaReferencedSchemasRecursive = (schemaName: string) => {
     const schema = openAPISpecification.components.schemas[schemaName];
     if (schema) {
-      if (
-        schema.type === 'object' &&
-        'properties' in schema &&
-        schema.properties
-      ) {
-        Object.values(schema.properties).forEach((property) => {
-          if (property) {
-            if ('oneOf' in property) {
-              property.oneOf.forEach((property) => {
-                if (property) {
-                  findSchemaReferencedBySchemaProperty(property);
-                }
-              });
-            } else {
-              findSchemaReferencedBySchemaProperty(property);
+      /**
+       * Traverses a schema object looking for $ref properties.
+       *
+       * @param schema The schema to traverse.
+       */
+      const traverseSchemaObject = (schema: Schema) => {
+        Object.keys(schema).forEach((schemaKey) => {
+          if ('$ref' in schema && schemaKey === '$ref') {
+            const schemaName = schema.$ref.replace('#/components/schemas/', '');
+            if (!schemaReferencedSchemas.includes(schemaName)) {
+              schemaReferencedSchemas.unshift(schemaName);
+              findSchemaReferencedSchemasRecursive(schemaName);
             }
           }
+          if (typeof (schema as any)[schemaKey] === 'object') {
+            traverseSchemaObject((schema as any)[schemaKey]);
+          }
         });
-      } else if (schema.type === 'array' && schema.items) {
-        if ('oneOf' in schema.items) {
-          schema.items.oneOf.forEach((property) => {
-            if (property) {
-              findSchemaReferencedBySchemaProperty(property);
-            }
-          });
-        } else {
-          findSchemaReferencedBySchemaProperty(schema.items);
-        }
-      }
+      };
+      traverseSchemaObject(schema);
     }
   };
   findSchemaReferencedSchemasRecursive(schemaName);
@@ -104,8 +64,18 @@ export const findSchemaReferencedSchemas = ({
 
 //#region Generate schema from request parameters
 export interface GenerateSchemaFromRequestParametersOptions {
+  /**
+   * The request parameters to generate a schema from.
+   */
   requestParameters: RequestParameter[];
 }
+
+/**
+ * Generate a schema from request parameters.
+ *
+ * @param options The options to generate a schema from request parameters.
+ * @returns The generated schema from the request parameters.
+ */
 export const generateSchemaFromRequestParameters = ({
   requestParameters,
 }: GenerateSchemaFromRequestParametersOptions) => {
@@ -128,13 +98,16 @@ export const generateSchemaFromRequestParameters = ({
         }
         return true;
       })
-      .reduce((accumulator, { name, schema, description }) => {
-        accumulator[name] = {
-          ...schema,
-          description,
-        };
-        return accumulator;
-      }, {} as Record<string, Schema>),
+      .reduce<Record<string, Schema>>(
+        (accumulator, { name, schema, description }) => {
+          accumulator[name] = {
+            ...schema,
+            description,
+          };
+          return accumulator;
+        },
+        {}
+      ),
     required: requestParameters
       .filter(({ required }) => {
         return required;
